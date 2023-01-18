70efd120efe7ba894226b9d890b881626e45bb71
BogAlex4ndr <bog.alex4ndr@gmail.com>
2023-01-18 14:29:18 +0200

    save

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import styles from '../styles/Main.module.css'
+import styles from '../styles/Main.module.css';
 
 const Main = () => {
-  return <div>Main fvff</div>;
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.container}>
+        <h1 className={styles.heading}>Join chat</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import './styles.css';
+import './styles/main.css';
 import App from './components/App';
 
 const conteiner = document.getElementById('root');
